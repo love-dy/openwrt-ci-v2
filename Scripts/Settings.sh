d2689e7846de4d6128ed3eb3217f33d98e05c671
love-dy <ilove11502@gmail.com>
2024-08-21 10:43:55 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -34,8 +34,8 @@ echo "CONFIG_PACKAGE_luci-app-design-config=y" >> .config
 
 #添加插件
 echo "CONFIG_PACKAGE_luci-app-adguardhome=y" >> ./.config
-#echo "CONFIG_PACKAGE_luci-app-ddns-go=y" >> ./.config
-#echo "CONFIG_PACKAGE_luci-app-alist=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-ddns-go=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-alist=y" >> ./.config
 
 #添加科学上网插件
 echo "CONFIG_PACKAGE_luci-app-passwall=y" >> ./.config
