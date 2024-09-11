19a658b45493dc6cbda765da48f0deaf1b253dc0
love-dy <ilove11502@gmail.com>
2024-09-11 08:36:38 +0800

    Update Patches.sh

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -2,8 +2,8 @@
 
 #LEDE源码默认为luci1分支版本，如需安装luci2分支版本的插件，需在编译前修改feeds.conf.default切换luci至openwrt-23.05分支。
 if [[ $WRT_URL == *"lede"* ]]; then
-	sed -i "/#src/d" ./wrt/feeds.conf.default
-	sed -i "s|\(src-git luci\).*|\1 https://github.com/coolsnowwolf/luci.git;openwrt-23.05|g" ./wrt/feeds.conf.default
+	#sed -i "/#src/d" ./wrt/feeds.conf.default
+	#sed -i "s|\(src-git luci\).*|\1 https://github.com/coolsnowwolf/luci.git;openwrt-23.05|g" ./wrt/feeds.conf.default
 
 	echo "$WRT_URL patch has been installed!"
 fi
